b27b7170060f9b804aaa7525b406964bcc1f6808
dragos-triteanu <dragos.triteanu@gmail.com>
2015-11-08 04:05:05 +0200

    Renamed package of platform to ro.management.platform

--- src/main/java/eu/accesa/platform/initializers/SpringSecurityInitializer.java
+++
@@ -1,5 +0,0 @@
-package eu.accesa.platform.initializers;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {}
