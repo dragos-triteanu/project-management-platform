fa5c5ae7495f8663db7c3476bda8f26d970a7569
Dragos Triteanu <dragos.triteanu@gmail.com>
2015-11-10 13:01:50 +0200

    Added hibernate db population with dummyData.

---
+++ src/main/resources/conf/SQLScripts/dummyData.sql
@@ -0,0 +1,3 @@
+INSERT INTO `projectmanagementdb`.`users` (`userId`,`mail`, `password`, `role`) VALUES (0,'admin', 'admin', 'ADMINISTRATOR');
+INSERT INTO `projectmanagementdb`.`users` (`userId`,`mail`, `password`, `role`) VALUES (1,'client', 'client', 'CLIENT');
+INSERT INTO `projectmanagementdb`.`clients` (`firstName`,`lastName`,`clientId`) VALUES ('someFirstName','someLastName',1);
